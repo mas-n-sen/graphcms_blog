9d3a15d87e83e8b00a3512265732cb1652eacd14
mas-n-sen <54278903+mas-n-sen@users.noreply.github.com>
2022-10-13 14:34:20 +0200

    Update index.js

--- services/index.js
+++ services/index.js
@@ -5,36 +5,27 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT ;
 export const getPosts = async () => {
     const query = gql `
     query MyQuery {
-        postsConnection {
-          edges {
-            node {
-              author {
-                bio
-                name
-                id
-                photo {
-                  url
-                }
-              }
-              slug
-              createdAt
-              title
-              excerpt
-              featuredImage {
-                url
-              }
-              categories {
-                name
-                slug
-              }
-            }
+      posts {
+        createdAt
+        excerpt
+        slug
+        title
+        author {
+          name
+          photo {
+            url
           }
         }
+        featuredImage {
+          url
+        }
       }
+    }
+
     `
     const result = await request(graphqlAPI, query);
 
-    return result.postsConnection;
+    return result.posts;
 }
 
 export const getPostDetails = async (slug) => {
