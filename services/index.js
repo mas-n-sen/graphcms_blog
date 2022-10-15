2434f9636326e1cd16f5eb239e39a4d08828ce01
mas-n-sen <azzedine.massinissa@gmail.com>
2022-10-15 21:47:22 +0200

    d commit

--- services/index.js
+++ services/index.js
@@ -3,35 +3,7 @@ import { request, gql } from "graphql-request";
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT ;
 
 export const getPosts = async () => {
-   /* const query = gql `
-    query MyQuery {
-        postsConnection {
-          edges {
-            node {
-              author {
-                bio
-                name
-                id
-                photo {
-                  url
-                }
-              }
-              slug
-              createdAt
-              title
-              excerpt
-              featuredImage {
-                url
-              }
-              categories {
-                name
-                slug
-              }
-            }
-          }
-        }
-      }
-    `*/
+   
     const query = gql `
     query MyQuery {
       posts {
